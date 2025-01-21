903a8593002f5179eb9d02adc8ecedf687530429
Sasha Sushko <sasha@sushko.me>
2025-01-21 10:55:34 +0200

    feat: scroll restoration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Outlet,
+  ScrollRestoration,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { AppHeader } from "@/app/layout";
@@ -6,10 +10,11 @@ import { CallToAction } from "@/widgets/call-to-action";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="container">
+    <div className="wrapper">
       <AppHeader />
       <Outlet />
       <CallToAction />
+      <ScrollRestoration />
       <TanStackRouterDevtools />
     </div>
   ),
